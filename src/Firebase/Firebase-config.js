68fcb889519d6a882c3d8323d116ea1b6fca5887
ProtyoyGhosh <pghosh0615@gmail.com>
2021-10-18 08:43:09 +0600

    checking

---
+++ src/Firebase/Firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcrKr85nLTuechMIZmEA0HV4Riug-Ql2g",
+    authDomain: "healthcare-assignment-245ae.firebaseapp.com",
+    projectId: "healthcare-assignment-245ae",
+    storageBucket: "healthcare-assignment-245ae.appspot.com",
+    messagingSenderId: "343725349272",
+    appId: "1:343725349272:web:5c02ff92905aeea4e5b3ed"
+};
+
+export default firebaseConfig;
